b85f5d98fc983373008bda681b24f7f72b8f2156
andermendz <andersonmendz19@gmail.com>
2024-07-23 20:12:40 -0500

    basic data usage

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import { getData } from "./weather";
 
 const submitButton = document.querySelector('#submit-button')
 const locationInput = document.querySelector('#location-input')
-const rawContent = document.querySelector('#raw-content')
+
+
+const informationElementLocation = document.getElementById('ie-location')
+const informationElementTimezone = document.getElementById('ie-timezone')
+const informationElementCurrentTemperature = document.getElementById('ie-current-temp')
 
 const query = await getData('cartagena')
 
-rawContent.innerHTML = query.resolvedAddress;
+informationElementLocation.innerHTML = query.resolvedAddress;
+informationElementTimezone.innerHTML = query.timezone;
+informationElementCurrentTemperature.innerHTML = query.currentConditions.temp + ' feels like ' + query.currentConditions.feelslike;
 
 function queryLocation(){
  
